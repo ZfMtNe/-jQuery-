800aef9cadbd4b2e0e8a094111708a742844e1e9
zmn <zheng2216257503@qq.com>
2023-03-25 00:14:46 +0800

    修改数量文本框可写值且添加直接修改数量值

--- index.js
+++ index.js
@@ -47,6 +47,16 @@
     const newNumber = +inp.val() + 1;
     setNumber(inp, newNumber);
   });
+  // 数量input监听
+  $("input").change(function () {
+    const inp = $(this);
+    const value = $(this).val();
+    if (!value || value == 0) {
+      $(this).val(1);
+    }
+    setNumber(inp, value);
+  });
+
   // 价格计算
   function setNumber(inp, number) {
     if (number < 1) {
